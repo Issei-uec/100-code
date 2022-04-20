a1f3f5d26e228d7c8372a56d69bfd087ebd6b7a9
{OKUDA <{eva02.vvv@gmail.com}>
2022-04-20 15:10:02 +0900

    commit

--- nock22.py
+++ nock22.py
@@ -1,11 +1,6 @@
 f = open('popular-names.txt', 'r')
 
 data = f.read()
-'''
-for i in data:
-    if i == '\t':
-        i = i.replace('\t', ' ')
-'''
 data1 = data.replace('\t', ' ')
 print(data1)
 f.close()
