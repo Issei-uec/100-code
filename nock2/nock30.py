7a847507a237357709542ecfb0c955e713d6bd48
{OKUDA <{eva02.vvv@gmail.com}>
2022-04-22 10:31:50 +0900

    commit

--- nock2/nock30.py
+++ nock2/nock30.py
@@ -1,5 +1,5 @@
 import pandas as pd
-datafile = pd.read_csv('popular-names.txt', sep="\t",header=None)
+datafile = pd.read_table('popular-names.txt', sep="\t", header=None)
 
 frequency = datafile[0].value_counts()
 print(frequency)
@@ -17,11 +17,7 @@ cut -f 1 -d " " test.txt > 19.txt
 sort 19.txt > 19_sort.txt
 uniq -c 19_sort.txt > 19_sort_uniq.txt
 sort -k 1 -t " " -r -n 19_sort_uniq.txt > 19_fin.txt
-    118 James
-    111 William
-    108 Robert
-    108 John
-     92 Mary
+
 
 リーダブルコード:
 変数の名前をわかりやすく
