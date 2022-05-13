e79ce79f5c0c62f3692612fe7076e44ac5f8b52f
{OKUDA <{eva02.vvv@gmail.com}>
2022-05-13 10:32:42 +0900

    commmit

--- UK.py
+++ UK.py
@@ -2,8 +2,8 @@ import gzip
 import json
 
 def text_output(text):
-    with gzip.open('jawiki-country.json.gz', mode='rb') as f:
-        for line in f:
+    with gzip.open('jawiki-country.json.gz', mode='rb') as file:
+        for line in file:
             article = json.loads(line)
             if article['title'] == text:
                 return article['text']
