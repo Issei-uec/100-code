7bd4e0788b1c695e0a2c5d3942cbaf392224e636
{OKUDA <{eva02.vvv@gmail.com}>
2022-05-19 16:03:01 +0900

    commit

--- nock30.py
+++ nock30.py
@@ -8,4 +8,14 @@ for line in neko:
         list.append({"surface": nekon[0], "base": nekon[7], "pos": nekon[1], "pos1": nekon[2]})
 neko.close()
 
-print(list[0])
+print(list[4:8])
+
+"""
+実行結果：
+[{'surface': '名前', 'base': '名前', 'pos': '名詞', 'pos1': '一般'}, {'surface': 'は', 'base': 'は', 'pos': '助詞', 'pos1': '係助
+詞'}, {'surface': 'まだ', 'base': 'まだ', 'pos': '副詞', 'pos1': '助詞類接続'}, {'surface': '無い', 'base': '無い', 'pos': '形容詞
+', 'pos1': '自立'}]
+
+リーダブルコード：
+
+"""
