e60570fa47ebe1615fb11ca153dfada28a827b15
{OKUDA <{eva02.vvv@gmail.com}>
2022-05-25 21:02:12 +0900

    commit

---
+++ nock40.py
@@ -0,0 +1,26 @@
+import re
+class Morph:
+  def __init__(self, sentence):
+    word = re.split(',|\t', sentence)
+    self.surface = word[0]
+    self.base = word[7]
+    self.pos = word[1]
+    self.pos1 = word[2]
+  
+sentence = []
+list = []
+AI = open("ai.ja.txt.parsed", "r", encoding="utf-8")
+for line in AI:
+  if line[0] != "*":
+    if line != "EOS\n":
+      list.append(Morph(line))
+    else:
+      sentence.append(list)
+      list = []
+  else:
+    continue
+
+AI.close()
+
+for morph in sentence[2]:
+    print(vars(morph))
